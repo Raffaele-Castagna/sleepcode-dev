1c5a2008d57530213a985854a46ca74ea74611d4
Raffaele Castagna <lele.castagna@libero.it>
2023-12-02 17:31:00 +0100

    added logo and sign-in button

---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,20 @@
+import Link from 'next/link';
+import React from 'react';
+
+type NavBarProps = {
+    
+};
+
+const NavBar:React.FC<NavBarProps> = () => {
+    
+    return <div className="flex items-center justify-between sm:px-12 px-2 md:px-24">
+        <Link href="/" className="flex items-center justify-center h-20">
+            <img src="/logo.svg" alt="SleepCode" className="h-full" />
+        </Link>
+            <div className="flex items-center">
+                <button className="bg-dark-fill-2 text-white px-2 py-1 sm:px-4 rounded-md text-sm font-medium
+                hover:text-brand-orange hover:bg-white hover:border-2 hover:border-brand-orange">Sign in</button>
+            </div>
+    </div>
+}
+export default NavBar;
---
+++ src/pages/auth/index.tsx
@@ -0,0 +1,14 @@
+import NavBar from '@/components/NavBar/NavBar';
+import React from 'react';
+
+type AuthPageProps = {
+    
+};
+
+const AuthPage:React.FC<AuthPageProps> = () => {
+    
+    return <div className="bg-dark-layer-2 h-screen relative">
+    <NavBar />
+    </div>
+}
+export default AuthPage;
